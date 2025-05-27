12d0956b2059cda8698368b8729984d25debf280
setruth <1607908758@qq.com>
2025-05-27 22:26:24 +0800

    feat: 新增首页动画

--- app/src/main/java/com/setruth/entityflowrecord/ui/pages/home/components/PeopleCountCard.kt
+++ app/src/main/java/com/setruth/entityflowrecord/ui/pages/home/components/PeopleCountCard.kt
@@ -1,5 +1,7 @@
 package com.setruth.entityflowrecord.ui.pages.home.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -21,6 +23,11 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -35,6 +42,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.setruth.entityflowrecord.model.ThemeMode
 import com.setruth.entityflowrecord.ui.theme.EntityFlowRecordTheme
+import kotlinx.coroutines.delay
 
 @Composable
 fun PeopleCountCard(
@@ -44,7 +52,16 @@ fun PeopleCountCard(
 
     val capacityUsage = currentPeople.toFloat() / maxCapacity
     val percentage = (capacityUsage * 100).toInt()
+    var progress by remember { mutableFloatStateOf(0f) }
 
+    LaunchedEffect(capacityUsage) {
+        delay(300)
+        progress = capacityUsage
+    }
+    val animatedValue by animateFloatAsState(
+        targetValue = progress,
+        animationSpec = tween(durationMillis = 1500),
+    )
     Card(
         modifier = Modifier.fillMaxWidth(),
         colors = CardDefaults.cardColors(
@@ -131,7 +148,7 @@ fun PeopleCountCard(
 
 
             LinearProgressIndicator(
-                progress = { capacityUsage },
+                progress = { animatedValue },
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(10.dp),
