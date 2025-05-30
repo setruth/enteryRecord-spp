cd52716b614f312a1cda68ca9430a9a057496292
setruth <1607908758@qq.com>
2025-05-30 15:30:50 +0800

    feat: 新增koin和蓝牙请求权限功能

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,8 @@ android {
 dependencies {
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.navigation.compose)
-
+    implementation(libs.koin.androidx.compose)
+    implementation(libs.koin.androidx.compose.navigation)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/setruth/entityflowrecord/MainActivity.kt
+++ app/src/main/java/com/setruth/entityflowrecord/MainActivity.kt
@@ -1,19 +1,75 @@
 package com.setruth.entityflowrecord
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.setruth.entityflowrecord.ui.frame.MainFrame
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.time.Duration.Companion.seconds
 
 class MainActivity : ComponentActivity() {
+    private val requestPermissionsLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val allGranted = permissions.entries.all { it.value }
+            if (allGranted) {
+
+            } else {
+                Toast.makeText(this, "无权限将3秒后退出软件", Toast.LENGTH_SHORT).show()
+                CoroutineScope(Dispatchers.IO).launch {
+                    delay(3.seconds)
+                    withContext(Dispatchers.Main) {
+                        finish()
+                    }
+                }
+            }
+        }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        requestBluetoothPermissions()
         setContent {
             MainFrame()
         }
     }
+    private fun requestBluetoothPermissions() {
+        val permissionsToRequest = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            arrayOf(
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        } else {
+            arrayOf(
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+
+        val neededPermissions = permissionsToRequest.filter {
+            ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+        }
+        if (neededPermissions.isNotEmpty()) {
+            requestPermissionsLauncher.launch(neededPermissions.toTypedArray())
+        }
+    }
+
 }
 
 
