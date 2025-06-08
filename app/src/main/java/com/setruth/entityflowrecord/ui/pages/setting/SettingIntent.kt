e8d8b3e44e480a211b17bb9ad9af590bbd0297c0
setruth <1607908758@qq.com>
2025-06-08 15:31:21 +0800

    feat: 完成设置页面的配置更新到硬件

--- app/src/main/java/com/setruth/entityflowrecord/ui/pages/setting/SettingIntent.kt
+++ app/src/main/java/com/setruth/entityflowrecord/ui/pages/setting/SettingIntent.kt
@@ -27,6 +27,8 @@ class SettingViewModel(
 ) : ViewModel() {
     private var _lightRange = MutableStateFlow(LightRange())
     val lightRange = _lightRange.asStateFlow()
+    private var _configUpdateState = MutableStateFlow<ConfigUpdateState>(ConfigUpdateState.None)
+    val configUpdateState = _configUpdateState.asStateFlow()
     private var _fullCount = MutableStateFlow(11)
     val fullCount = _fullCount.asStateFlow()
     private var _voiceRemindEnable = MutableStateFlow(false)
@@ -61,27 +63,100 @@ class SettingViewModel(
 
     fun sendIntent(intent: SettingViewIntent) = when (intent) {
         is SettingViewIntent.UpdateLightRange -> {
-            _lightRange.update { intent.lightRange }
-            mmkv?.putInt(ConfigKeys.YELLOW_LIGHT_RANGE, _lightRange.value.warn)
-            mmkv?.putInt(ConfigKeys.RED_LIGHT_RANGE, _lightRange.value.err)
+            updateLightRange(intent.lightRange)
         }
 
         is SettingViewIntent.UpdateFullCount -> {
-            _fullCount.update { intent.fullCount }
-            mmkv?.putInt(ConfigKeys.FULL_COUNT, _fullCount.value)
+            updateFullCount(intent.fullCount)
         }
 
         is SettingViewIntent.UpdateNoEntranceEnable -> {
-            _noEntranceEnable.update { intent.enable }
-            mmkv?.putBoolean(ConfigKeys.NO_ENTRANCE_ENABLE, _noEntranceEnable.value)
+            updateNoEntranceEnable(intent.enable)
 
         }
 
         is SettingViewIntent.UpdateVoiceRemindEnable -> {
-            _voiceRemindEnable.update { intent.enable }
-            mmkv?.putBoolean(ConfigKeys.VOICE_REMIND_ENABLE, _voiceRemindEnable.value)
+            updateVoiceRemindEnable(intent.enable)
         }
 
     }
 
+   private fun updateFullCount(newCount: Int) = sendCMDHandle(
+        onSendBlock = {
+            cmdBTRepository.setNewMaxCount(newCount)
+        },
+        onSuccess = {
+            _fullCount.update { newCount }
+            mmkv?.putInt(ConfigKeys.FULL_COUNT, _fullCount.value)
+        },
+    )
+
+    private fun updateVoiceRemindEnable(enable: Boolean) = sendCMDHandle(
+        onSendBlock = {
+            if (enable) {
+                cmdBTRepository.enableBuzzer()
+            } else {
+                cmdBTRepository.disableBuzzer()
+            }
+        },
+        onSuccess = {
+            _voiceRemindEnable.update { enable }
+            mmkv?.putBoolean(ConfigKeys.VOICE_REMIND_ENABLE, _voiceRemindEnable.value)
+        },
+    )
+
+    private  fun updateNoEntranceEnable(enable: Boolean) = sendCMDHandle(
+        onSendBlock = {
+            if (enable) {
+                cmdBTRepository.enableFullStop()
+            } else {
+                cmdBTRepository.disableFullStop()
+            }
+        },
+        onSuccess = {
+            _noEntranceEnable.update { enable }
+            mmkv?.putBoolean(ConfigKeys.NO_ENTRANCE_ENABLE, _noEntranceEnable.value)
+        },
+    )
+
+    private fun updateLightRange(lightRange: LightRange) {
+        sendCMDHandle(
+            onSendBlock = {
+                if (lightRange.warn != _lightRange.value.warn && lightRange.err != _lightRange.value.err) {
+                    cmdBTRepository.setNewAlarmAndErrorLight(lightRange.warn, lightRange.err)
+                } else if (lightRange.warn != _lightRange.value.warn) {
+                    cmdBTRepository.setNewAlarmLight(lightRange.warn)
+                } else if (lightRange.err != _lightRange.value.err) {
+                    cmdBTRepository.setNewErrorLight(lightRange.err)
+                } else true
+            },
+            onSuccess = {
+                _lightRange.update { lightRange }
+                mmkv?.putInt(ConfigKeys.YELLOW_LIGHT_RANGE, _lightRange.value.warn)
+                mmkv?.putInt(ConfigKeys.RED_LIGHT_RANGE, _lightRange.value.err)
+            }
+        )
+    }
+
+    private fun sendCMDHandle(
+        onSendBlock: () -> Boolean,
+        onSuccess: () -> Unit,
+        onFailed: () -> Unit = {}
+    ) {
+        _configUpdateState.value = ConfigUpdateState.Loading
+        if (onSendBlock()) {
+            onSuccess();
+            _configUpdateState.value = ConfigUpdateState.Success
+        } else {
+            onFailed();
+            _configUpdateState.value = ConfigUpdateState.Failed
+        }
+    }
+}
+
+sealed class ConfigUpdateState {
+    object None : ConfigUpdateState()
+    object Loading : ConfigUpdateState()
+    object Success : ConfigUpdateState()
+    object Failed : ConfigUpdateState()
 }
--- app/src/main/java/com/setruth/entityflowrecord/ui/pages/setting/SettingView.kt
+++ app/src/main/java/com/setruth/entityflowrecord/ui/pages/setting/SettingView.kt
@@ -2,17 +2,27 @@ package com.setruth.entityflowrecord.ui.pages.setting
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,7 +33,10 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.setruth.entityflowrecord.data.di.appModule
 import com.setruth.entityflowrecord.data.model.ThemeMode
+import com.setruth.entityflowrecord.data.repository.BluetoothConnectionState
 import com.setruth.entityflowrecord.data.repository.BluetoothRepository
+import com.setruth.entityflowrecord.data.repository.CMDBTRepository
+import com.setruth.entityflowrecord.data.repository.InitConfigState
 import com.setruth.entityflowrecord.ui.frame.MainFrame
 import com.setruth.entityflowrecord.ui.pages.setting.components.FullCountCard
 import com.setruth.entityflowrecord.ui.pages.setting.components.OtherSettingsCard
@@ -43,8 +56,22 @@ fun SettingView(
     val voiceRemindEnable by viewModel.voiceRemindEnable.collectAsState()
     val noEntranceEnable by viewModel.noEntranceEnable.collectAsState()
     val scope = rememberCoroutineScope()
+    val configUpdateState by viewModel.configUpdateState.collectAsState()
     val snackbarHostState = remember { SnackbarHostState() }
+    LaunchedEffect(configUpdateState) {
+        when (configUpdateState) {
+            is ConfigUpdateState.Success -> {
+                scope.launch {
+                    snackbarHostState.showSnackbar(message = "更新成功")
+                }
+            }
 
+            is ConfigUpdateState.Failed -> {
+                snackbarHostState.showSnackbar(message = "更新失败,请稍后重试")
+            }
+            else -> {}
+        }
+    }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -52,6 +79,29 @@ fun SettingView(
             SnackbarHost(hostState = snackbarHostState)
         }
     ) { _ ->
+
+        if (configUpdateState == ConfigUpdateState.Loading) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.width(64.dp),
+                        color = MaterialTheme.colorScheme.secondary,
+                        trackColor = MaterialTheme.colorScheme.surfaceVariant,
+                    )
+                    Spacer(modifier = Modifier.height(30.dp))
+                    Text(
+                        text = "正在更新配置",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    )
+                }
+                return@Scaffold
+            }
+        }
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(10.dp)
@@ -59,18 +109,11 @@ fun SettingView(
             item {
                 FullCountCard(fullCount = fullCount) {
                     viewModel.sendIntent(SettingViewIntent.UpdateFullCount(it))
-                    scope.launch {
-                        snackbarHostState.showSnackbar(message = "正在设置最大容量")
-
-                    }
                 }
             }
             item {
                 ThresholdConfigCard(lightRange = lightRange) {
                     viewModel.sendIntent(SettingViewIntent.UpdateLightRange(it))
-                    scope.launch {
-                        snackbarHostState.showSnackbar(message = "正在设置阈值")
-                    }
                 }
             }
             item {
@@ -86,6 +129,7 @@ fun SettingView(
                 )
             }
         }
+
     }
 
 }
@@ -97,7 +141,7 @@ fun SettingViewPreview() {
     EntityFlowRecordTheme(ThemeMode.LIGHT) {
         KoinApplicationPreview(application = {
             modules(appModule, module {
-                single { BluetoothRepository() }
+                single { CMDBTRepository() }
             })
         }) {
             MainFrame(2)
